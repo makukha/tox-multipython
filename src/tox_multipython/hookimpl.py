65747f9aebab5e6d3ef95a98246ea2b51ef43762
Michael Makukha <m.makukha@gmail.com>
2025-01-12 10:49:40 +0200

    Use 'py bin'

--- src/tox_multipython/hookimpl.py
+++ src/tox_multipython/hookimpl.py
@@ -1,12 +1,17 @@
-import os.path
 import re
+import sys
+from subprocess import check_output
+
+try:
+    from typing import TYPE_CHECKING, Union
+except ImportError:
+    TYPE_CHECKING = False
 
 import pluggy
 
 
 hookimpl = pluggy.HookimplMarker('tox')
 
-MULTIPYTHON_PATH_ROOT = '/usr/local/bin'
 RX = (
     re.compile(r'^(?P<impl>py)(?P<maj>[23])(?P<min>[0-9][0-9]?)$'),
     re.compile(r'^(?P<impl>py)(?P<maj>3)(?P<min>[0-9][0-9])(?P<suffix>t)$'),
@@ -16,21 +21,23 @@ RX = (
 @hookimpl
 def tox_get_python_executable(envconfig):  # type: ignore
     """Return a python executable for the given python base name."""
-    match = None
     for rx in RX:
         match = rx.match(envconfig.envname)
         if match is not None:
-            break
+            return get_python_path(envconfig.envname)
+    return None
 
-    if match is None:
+
+def get_python_path(tag):  # type: (str) -> Union[str, None]
+    # get path
+    try:
+        # ruff: noqa: S603 = allow check_output with arbitrary cmdline
+        # ruff: noqa: S607 = py is on path, specific location is not guaranteed
+        out = check_output(['py', 'bin', '--path', tag])
+        enc = sys.getfilesystemencoding()
+        path = (out.decode() if enc is None else out.decode(enc)).strip()
+        if not path:
+            return None
+    except Exception:
         return None
-
-    g = match.groupdict()
-    g['suffix'] = g.get('suffix', '')
-    name = {'py': 'python'}[g['impl']]
-    command = '{name}{maj}.{min}{suffix}'.format(name=name, **g)
-    proposed = MULTIPYTHON_PATH_ROOT + '/' + command
-    if not os.path.exists(proposed):
-        proposed = None  # type: ignore[assignment]
-
-    return proposed
+    return path
